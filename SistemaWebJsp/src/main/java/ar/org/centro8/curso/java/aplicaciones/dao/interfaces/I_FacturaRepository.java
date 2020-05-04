aff2a9e34fdcf140b1a3c0c43b7c749f0c051e23
toro <toro@192.168.0.17>
2020-05-04 17:30:29 -0300

    Ajustes

--- SistemaWebJsp/src/main/java/ar/org/centro8/curso/java/aplicaciones/dao/interfaces/I_FacturaRepository.java
+++ SistemaWebJsp/src/main/java/ar/org/centro8/curso/java/aplicaciones/dao/interfaces/I_FacturaRepository.java
@@ -43,12 +43,11 @@ public interface I_FacturaRepository {
                 .orElse(new Factura());
     }
     
-//    default List<Factura> getByIdCliente(int id){
-//        return getAll()
-//                .stream()
-//                .filter(f -> f.getIdCliente() == id)
-//                .collect(Collectors.toList());
-//                
-//    }
+    default List<Factura> getByIdCliente(int id){
+        return getAll()
+                .stream()
+                .filter(f -> f.getIdCliente() == id)
+                .collect(Collectors.toList());                
+    }
     
 }
