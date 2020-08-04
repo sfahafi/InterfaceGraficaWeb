b0db70752ae1f1efbc81c43c85aa5e430b3b28d2
toro <toro@192.168.0.17>
2020-08-03 23:45:43 -0300

    Cambio DB

--- SistemaWebJsp/src/main/java/ar/org/centro8/curso/java/aplicaciones/dao/jdbc/DetalleRepository.java
+++ SistemaWebJsp/src/main/java/ar/org/centro8/curso/java/aplicaciones/dao/jdbc/DetalleRepository.java
@@ -14,7 +14,7 @@ public class DetalleRepository implements I_DetalleRepository{
     public void save(Detalle detalle) {
         if(detalle == null) return;
         try (PreparedStatement ps = conn.prepareStatement(
-                "insert into detalles (idFactura,idArticulo,precio,cantidad)"
+                "insert into detalle (idFactura,idArticulo,precio,cantidad)"
                         + "values (?,?,?,?)",
                 PreparedStatement.RETURN_GENERATED_KEYS)
             ){
@@ -33,7 +33,7 @@ public class DetalleRepository implements I_DetalleRepository{
     public void remove(Detalle detalle) {
         if(detalle == null) return;
         try (PreparedStatement ps = conn.prepareStatement(
-                "delete from detalles where id=?")
+                "delete from detalle where id=?")
             ){
             
         } catch (Exception e) { e.printStackTrace(); }
@@ -43,7 +43,7 @@ public class DetalleRepository implements I_DetalleRepository{
     public void update(Detalle detalle) {
         if(detalle == null) return;
         try (PreparedStatement ps = conn.prepareStatement(
-                "update detalles set  where id=?")
+                "update detalle set  where id=?")
             ){
             
         } catch (Exception e) { e.printStackTrace(); }
@@ -53,7 +53,7 @@ public class DetalleRepository implements I_DetalleRepository{
     public List<Detalle> getAll() {
         List<Detalle> list = new ArrayList();
         try (ResultSet rs = conn.createStatement().executeQuery(
-                "select * from detalles")
+                "select * from detalle")
             ){
             
             while (rs.next()){
--- SistemaWebJsp/src/main/java/ar/org/centro8/curso/java/aplicaciones/test/TestClienteRepository.java
+++ SistemaWebJsp/src/main/java/ar/org/centro8/curso/java/aplicaciones/test/TestClienteRepository.java
@@ -14,21 +14,21 @@ public class TestClienteRepository {
             
             System.out.println("\n" + "++++++++++++++++++++++++++++++++++++++++++++++++++++++++" + "\n");
             
-            cr.remove(cr.getByTipoNumero(TipoDocumento.DNI, 16777888));
-            System.out.println("Cliente con DNI: 16777888, eliminado !!!");
+           cr.remove(cr.getByTipoNumero(TipoDocumento.DNI, 95959595));
+//            System.out.println("Cliente con DNI: 16777888, eliminado !!!");
             
             System.out.println("\n" + "++++++++++++++++++++++++++++++++++++++++++++++++++++++++" + "\n");
             
-            Cliente cliente = new Cliente("Juan","Sosa",TipoDocumento.DNI,16777888,"Av. Tal","Hola");
+            Cliente cliente = new Cliente("Sharbel", "Fahafi", TipoDocumento.DNI, 95723542, "flores", "Spring Boot Test");
             cr.save(cliente);
             System.out.println(cliente + "\nAgregado a la base");
             
             System.out.println("\n" + "++++++++++++++++++++++++++++++++++++++++++++++++++++++++" + "\n");
             
-//            cliente = (Cliente) cr.getLikeNombreApellido("juan", "sosa"); 
+//            cliente = (Cliente) cr.getLikeApellido("FAhafi");
 //            System.out.println("cliente = " + cliente);
-//            cliente.setNombre("Pedro");
-//            cliente.setApellido("Suarez");
+//            //cliente.setNombre("Pedro");
+//            cliente.setApellido("Fahafi");
 //            cr.update(cliente);
 //            System.out.println("cliente = " + cliente);            
             
@@ -38,11 +38,11 @@ public class TestClienteRepository {
             
             System.out.println("\n" + "++++++++++++++++++++++++++++++++++++++++++++++++++++++++" + "\n");
             
-            cr.getLikeApellido("sot").forEach(System.out::println);
+            //cr.getLikeApellido("sot").forEach(System.out::println);
             
             System.out.println("\n" + "++++++++++++++++++++++++++++++++++++++++++++++++++++++++" + "\n");
             
-            cr.getLikeNombreApellido("lu", "gu").forEach(System.out::println);
+            //cr.getLikeNombreApellido("lu", "gu").forEach(System.out::println);
             
         } catch (Exception e) { e.printStackTrace(); }
                 
