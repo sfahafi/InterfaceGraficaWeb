9c5845ff4d4067be26d69d7bae04b6c72a04dd1b
Administrador <Administrador@PC>
2020-08-04 12:32:44 -0300

    ajuste db

--- SistemaWebJsf/src/main/java/ar/org/centro8/curso/java/aplicaciones/sql/comandos.sql
+++ SistemaWebJsf/src/main/java/ar/org/centro8/curso/java/aplicaciones/sql/comandos.sql
@@ -1,8 +1,16 @@
 use negocioWeb;
 use S36WIHK7x4;
-select * from clientes;
-select * from facturas;
-select * from articulos;
+select * from cliente;
+select * from factura;
+select * from articulo;
+select * from detalle;
+
+describe cliente;
+describe factura;
+describe articulo;
+describe detalle;
+
+delete from factura where id = 1;
 
 delete from articulos where descripcion = 'Juguetes';
 
@@ -13,3 +21,8 @@ delete from articulos where descripcion = 'Juguetes';
 -- update	empleados
 -- set		sueldo = sueldo * 1.08
 -- where	sueldo < 5000;
+
+insert into detalle(idFactura,idArticulo,precio,cantidad) values 
+    (2,1,3578,50),
+    (3,2,4353,5546),
+    (4,5,5464,453);
